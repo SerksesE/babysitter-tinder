b7f99d36497a4e4eaf969ea19d9731ed3bb1ea96
lvsanh <lvsanh@dtvt.org>
2018-11-09 11:05:17 +0100

    change parents profile css and remove some files

--- src/components/ParentsContainer.js
+++ src/components/ParentsContainer.js
@@ -7,31 +7,34 @@ export class ParentsContainer extends Component {
         parents: [
             {
                 id: 1,
-                userName: 'Beckham',
+                userName: 'Tanja',
                 userPassword: '123456',
                 ageOfChildren: 2,
                 numberOfChildren: 1,
                 feeInEuros: 12,
                 experienceInYears: 2,
-                locationAms: 'yes'
+                locationAms: 'yes',
+                description: 'Hi! I am living in Amsterdam and I want to find a babysitter to takecare my lovely angel. She is 2 years old' 
             }, {
                 id: 2,
-                userName: 'Messi',
+                userName: 'Anna',
                 userPassword: '123456',
                 ageOfChildren: 1,
                 numberOfChildren: 2,
                 feeInEuros: 6,
                 experienceInYears: 3,
-                locationAms: 'yes'
+                locationAms: 'yes',
+                description: 'Hi! I am living in Amsterdam and I want to find a babysitter to takecare my lovely angel. She is 1 years old' 
             }, {
                 id: 3,
-                userName: 'Rooney',
+                userName: 'Jenny',
                 userPassword: '123456',
                 ageOfChildren: 3,
                 numberOfChildren: 3,
                 feeInEuros: 12,
                 experienceInYears: 5,
-                locationAms: 'yes'
+                locationAms: 'yes',
+                description: 'Hi! I am living in Amsterdam and I want to find a babysitter to takecare my lovely angel. She is 3 years old'
             }, {
                 id: 4,
                 userName: 'De Jong',
@@ -40,16 +43,18 @@ export class ParentsContainer extends Component {
                 numberOfChildren: 4,
                 feeInEuros: 8,
                 experienceInYears: 8,
-                locationAms: 'yes'
+                locationAms: 'yes',
+                description: 'Hi! I am living in Amsterdam and I want to find a babysitter to takecare my lovely angel. She is 4 years old' 
             }, {
                 id: 5,
                 userName: 'Jane',
                 userPassword: '123456',
-                ageOfChildren: 12,
+                ageOfChildren: 3,
                 numberOfChildren: 2,
                 feeInEuros: 15,
                 experienceInYears: 3,
-                locationAms: 'yes'
+                locationAms: 'yes',
+                description: 'Hi! I am living in Amsterdam and I want to find a babysitter to takecare my lovely angel. She is 3 years old'
             }, {
                 id: 6,
                 userName: 'Joop',
@@ -58,7 +63,8 @@ export class ParentsContainer extends Component {
                 numberOfChildren: 1,
                 feeInEuros: 11,
                 experienceInYears: 1,
-                locationAms: 'yes'
+                locationAms: 'yes',
+                description: 'Hi! I am living in Amsterdam and I want to find a babysitter to takecare my lovely angel. She is 7 years old'
             }, {
                 id: 7,
                 userName: 'Holla',
@@ -67,34 +73,38 @@ export class ParentsContainer extends Component {
                 numberOfChildren: 1,
                 feeInEuros: 14,
                 experienceInYears: 1,
-                locationAms: 'yes'
+                locationAms: 'yes',
+                description: 'Hi! I am living in Amsterdam and I want to find a babysitter to takecare my lovely angel. She is 7 years old' 
             }, {
                 id: 8,
                 userName: 'Edo',
                 userPassword: '123456',
-                ageOfChildren: 7,
+                ageOfChildren: 2,
                 numberOfChildren: 1,
                 feeInEuros: 10,
                 experienceInYears: 1,
-                locationAms: 'yes'
+                locationAms: 'yes',
+                description: 'Hi! I am living in Amsterdam and I want to find a babysitter to takecare my lovely angel. She is 2 years old' 
             }, {
                 id: 9,
                 userName: 'Peter',
                 userPassword: '123456',
-                ageOfChildren: 7,
+                ageOfChildren: 1,
                 numberOfChildren: 1,
                 feeInEuros: 12,
                 experienceInYears: 1,
-                locationAms: 'yes'
+                locationAms: 'yes',
+                description: 'Hi! I am living in Amsterdam and I want to find a babysitter to takecare my lovely angel. She is 1 years old' 
             }, {
                 id: 10,
                 userName: 'Mario',
                 userPassword: '123456',
-                ageOfChildren: 7,
+                ageOfChildren: 5,
                 numberOfChildren: 1,
                 feeInEuros: 15,
                 experienceInYears: 1,
-                locationAms: 'yes'
+                locationAms: 'yes',
+                description: 'Hi! I am living in Amsterdam and I want to find a babysitter to takecare my lovely angel. She is 5 years old'
             }
         ]
     }
@@ -108,12 +118,13 @@ export class ParentsContainer extends Component {
                 expYears={parents.experienceInYears}
                 fee={parents.feeInEuros}
                 location={parents.locationAms}
+                description={parents.description}
             />
         )
     }
 
     render() {
-        const i = Math.ceil(Math.random() * 10)
+        const i = Math.floor(Math.random() * this.state.parents.length)
         return (
             <div className="parentsContainer">
                 {this.state.parents
